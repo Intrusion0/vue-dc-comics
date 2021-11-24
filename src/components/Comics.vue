fb075c516efd1ac54782ebbc5280d87f3a2f4adc
Mario <lombardomario01@gmail.com>
2021-11-24 18:05:20 +0100

    Bonus completed

--- src/components/Comics.vue
+++ src/components/Comics.vue
@@ -7,57 +7,18 @@
                 </span>
             </div>
         </section>
-        <section id="icons-main">
-            <div class="ms-container">
-                <nav>
-                    <ul>
-                        <li v-for="comicsEl, j in comicsElements" :key="j">
-                            <a :href="comicsEl.link">
-                                <img :src="require(`@/assets/img/${comicsEl.picture}`)" :alt="comicsEl.text">
-                                <span>DIGITAL COMICS</span>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </section>
+        <IconsComics/>
     </div>
 </template>
 
 <script>
+import IconsComics from './IconsComics.vue'
+
 export default {
   name: 'Comics',
-  data() {
-      return {
-          comicsElements: [
-            {
-                text: 'DIGITAL COMICS',
-                link: '#',
-                picture: 'buy-comics-digital-comics.png'
-            },
-            {
-                text: 'DC MERCHANDISE',
-                link: '#',
-                picture: 'buy-comics-merchandise.png'
-            },
-            {
-                text: 'SUBSCRIPTION',
-                link: '#',
-                picture: 'buy-comics-subscriptions.png'
-            },
-            {
-                text: 'COMICS SHOP LOCATOR',
-                link: '#',
-                picture: 'buy-comics-shop-locator.png'
-            },
-            {
-                text: 'DC POWER VISA',
-                link: '#',
-                picture: 'buy-dc-power-visa.svg'
-            }
-          ]
-        }
-    }   
+  components: {
+      IconsComics
+  }
 }
 </script>
 
@@ -75,41 +36,6 @@ div.comics {
             color: white;
             font-size: 22px;
             font-weight: 600;
-        }
-    }
-
-    #icons-main {
-        padding: 50px 0;
-        width: 100%;
-        background-color: #0282f9;
-
-        nav {
-
-            ul {
-                display: flex;
-                list-style: none;
-
-                li {
-                    margin: 0 29px;
-
-                    a {
-                        display: flex;
-                        align-items: center;
-                        text-decoration: none;
-                        color: #fff;
-                        font-size: 14px;
-
-                        img {
-                            width: 60px;
-                            height: 65px;
-                        }
-
-                        span {
-                            margin-left: 15px;
-                        }
-                    }
-                }
-            }
         }
     }
 }
